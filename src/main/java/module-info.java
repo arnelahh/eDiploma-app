205f5ac619014deedfe448c46094a7c6aa99cc16
arnelahh <arnela.hodzic.23@size.ba>
2025-12-11 18:48:01 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module main.ediploma {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens main.ediploma to javafx.fxml;
+    exports main.ediploma;
+}
