1a3f1333ef194f028d10998ad9be4acad4cac9ee
arnelahh <arnela.hodzic.23@size.ba>
2025-12-12 12:46:04 +0100

    #5 - CommissionRole

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -2,6 +2,7 @@ package model;
 
 import dao.AcademicStaffDAO;
 import dao.AppUserDAO;
+import dao.CommissionRoleDAO;
 import dao.SubjectDAO;
 
 import java.util.List;
@@ -14,18 +15,24 @@ public class Main {
         for (Subject subject : predmeti) {
             System.out.println(subject);
         }
-
+        System.out.println("--------------------------");
         AppUserDAO dao = new AppUserDAO();
         var users = dao.getAllUsers();
-
         users.forEach(System.out::println);
 
+        System.out.println("--------------------------");
         AcademicStaffDAO academicStaffDAO = new AcademicStaffDAO();
         List<AcademicStaff> staff = academicStaffDAO.getAllAcademicStaff();
         for(AcademicStaff as : staff){
             System.out.println(as);
         }
 
+        System.out.println("--------------------------");
+        CommissionRoleDAO commissionRoleDAO = new CommissionRoleDAO();
+        List<CommissionRole> roles = commissionRoleDAO.getCommissionRoles();
+        for(CommissionRole role : roles){
+            System.out.println(role);
+        }
     }
 
 }
