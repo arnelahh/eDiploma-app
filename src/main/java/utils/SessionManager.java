233c97e52bd309d0af6f9618cbbc37a8265e5df5
ErminStarcevic <ermin.starcevic.23@size.ba>
2025-12-12 23:18:55 +0100

    Created session for login

---
+++ src/main/java/utils/SessionManager.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SessionManager {
+    private static Timer timer;
+    private static final long SESSION_TIMEOUT = 2 * 60 * 60 * 1000;
+
+    private static Runnable onSessionExpiredCallback;
+
+    public static void startSession(Runnable onSessionExpired) {
+        onSessionExpiredCallback = onSessionExpired;
+        resetTimer();
+    }
+
+    public static void resetTimer() {
+        if(timer != null) {
+            timer.cancel();
+        }
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(onSessionExpiredCallback != null) {
+                    onSessionExpiredCallback.run();
+                }
+            }
+        }, SESSION_TIMEOUT);
+    }
+
+    public static void stopSession() {
+        if(timer != null) timer.cancel();
+        UserSession.clear();
+    }
+}
