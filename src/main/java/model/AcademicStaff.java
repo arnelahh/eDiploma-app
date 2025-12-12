5df5abc8c07e2c837473e3117483a50f0a8ef585
arnelahh <arnela.hodzic.23@size.ba>
2025-12-12 11:20:18 +0100

    #3 - lombok integrated

--- src/main/java/model/AcademicStaff.java
+++ src/main/java/model/AcademicStaff.java
@@ -1,7 +1,10 @@
 package model;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class AcademicStaff {
     private int Id;
     private String Title;
@@ -12,103 +15,4 @@ public class AcademicStaff {
     private boolean IsActive;
     private LocalDateTime CreatedAt;
     private LocalDateTime UpdatedAt;
-
-    public AcademicStaff(){}
-
-    public AcademicStaff(int id, String title, String firstName, String lastName, String email, boolean isDean, boolean isActive, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        Id = id;
-        Title = title;
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        IsDean = isDean;
-        IsActive = isActive;
-        CreatedAt = createdAt;
-        UpdatedAt = updatedAt;
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName) {
-        FirstName = firstName;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public boolean isDean() {
-        return IsDean;
-    }
-
-    public void setDean(boolean dean) {
-        IsDean = dean;
-    }
-
-    public boolean isActive() {
-        return IsActive;
-    }
-
-    public void setActive(boolean active) {
-        IsActive = active;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return CreatedAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        CreatedAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return UpdatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        UpdatedAt = updatedAt;
-    }
-
-    @Override
-    public String toString() {
-        return "AcademicStaff{" +
-                "Id=" + Id +
-                ", Title='" + Title + '\'' +
-                ", FirstName='" + FirstName + '\'' +
-                ", LastName='" + LastName + '\'' +
-                ", Email='" + Email + '\'' +
-                ", IsDean=" + IsDean +
-                ", IsActive=" + IsActive +
-                '}';
-    }
 }
