55cf1b8c420a0d7106d70ac5467017b0d423a990
arnelahh <arnela.hodzic.23@size.ba>
2025-12-13 22:18:46 +0100

    Fullscreen enabled
    molimo Allaha Milosnog da jest

--- src/main/java/utils/SceneManager.java
+++ src/main/java/utils/SceneManager.java
@@ -1,8 +1,10 @@
 package utils;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class SceneManager {
@@ -13,19 +15,27 @@ public class SceneManager {
     // poziva se JEDNOM na startu aplikacije
     public static void init(Stage stage) {
         primaryStage = stage;
-        primaryStage.setMaximized(true);
-    }
 
+        // Dobijamo rezoluciju korisnika
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+
+        primaryStage.setX(screenBounds.getMinX());
+        primaryStage.setY(screenBounds.getMinY());
+        primaryStage.setWidth(screenBounds.getWidth());
+        primaryStage.setHeight(screenBounds.getHeight());
+
+        primaryStage.show();
+    }
     public static void show(String fxmlPath, String title) {
         try {
-            FXMLLoader loader = new FXMLLoader(
-                    SceneManager.class.getResource(fxmlPath)
-            );
+            FXMLLoader loader = new FXMLLoader(SceneManager.class.getResource(fxmlPath));
             Parent root = loader.load();
-
-            primaryStage.setScene(new Scene(root));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
             primaryStage.setTitle(title);
-            primaryStage.setMaximized(true); // centralno
+            if (!primaryStage.isMaximized()) {
+                primaryStage.setMaximized(true);
+            }
             primaryStage.show();
 
         } catch (Exception e) {
